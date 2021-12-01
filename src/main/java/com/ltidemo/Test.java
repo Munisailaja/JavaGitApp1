4187bc809d13d28db7ba7ff5dcc29300e0d0dcd7
Munisailaja <munigsailaja@gmail.com>
2021-12-01 15:11:24 +0530

    Second commit

---
+++ src/main/java/com/ltidemo/Test.java
@@ -0,0 +1,5 @@
+package com.ltidemo;
+
+public class Test {
+
+}
--- src/main/java/com/ltidemo/UserApp.java
+++ src/main/java/com/ltidemo/UserApp.java
@@ -4,6 +4,7 @@ public class UserApp {
 	public static void main(String[] args) {
 		Product p1 = new Product(10, "Hellp");
 		System.out.println(p1);
+		System.out.println( "welcome");
 	}
 
 }
